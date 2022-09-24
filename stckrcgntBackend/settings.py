90b65411a91c9f95b11e91967ed077a02710b199
juanCR44 <juancr.96@hotmail.com>
2022-09-24 00:19:00 -0500

    update

--- stckrcgntBackend/settings.py
+++ stckrcgntBackend/settings.py
@@ -33,8 +33,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 #STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-#STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'api.apps.ApiConfig',
     'corsheaders',
@@ -124,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-#STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
